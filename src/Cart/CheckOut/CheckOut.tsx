86f2427b03bfa9f7a26bb72e2d77902374a69d09
Maximilian Kaske <maximilian@kaske.org>
2020-09-07 16:51:32 +0200

    :wrench:
    better transition sequence implementation

--- src/Cart/CheckOut/CheckOut.tsx
+++ src/Cart/CheckOut/CheckOut.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import { View } from "react-native";
 import { CartNavigationProps } from "../../components/Navigation";
+import {
+  Transition,
+  Transitioning,
+  TransitioningView
+} from "react-native-reanimated";
+import { Box } from "../../components";
+
+const transition = (
+  <Transition.Sequence>
+    <Transition.Change />
+    <Transition.Out type="slide-right" interpolation="easeInOut" />
+  </Transition.Sequence>
+);
 
 const CheckOut = ({ navigation }: CartNavigationProps<"CheckOut">) => {
-  return <View style={{ flex: 1 }}></View>;
+  const ref = useRef<TransitioningView>(null);
+  return (
+    <Box flex={1} backgroundColor="mainBackground">
+      <Transitioning.View {...{ ref, transition }}></Transitioning.View>
+    </Box>
+  );
 };
 
 export default CheckOut;
